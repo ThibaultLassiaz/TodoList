3c21285ea33635f47e47e4d1752601330bbfa3d2
machadod <dylan.machado@etu.univ-grenoble-alpes.fr>
2017-12-06 15:02:26 +0100

    ajout message erreur dans formulaire tâche

--- TodoListClientAngular2/src/app/task-modal/task-modal.component.ts
+++ TodoListClientAngular2/src/app/task-modal/task-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, Input } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {ListID, ItemJSON,  TodoListService} from "../todo-list.service";
 import {dataForItem} from "../../data/protocol";
+import {isUndefined} from "util";
 
 @Component({
   selector: 'app-task-modal',
@@ -40,8 +41,9 @@ export class TaskModalComponent implements OnInit {
 })
 export class TaskModalComponentForm {
 
-  @Input() date: Date;
+  @Input() date: Date = new Date();
   public pined:boolean = false;
+  public errorMessage = "";
 
   constructor(
     public dialogRef: MatDialogRef<TaskModalComponentForm>,
@@ -49,22 +51,40 @@ export class TaskModalComponentForm {
 
 
 
+
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  public getErrorMessage() : string{
+    return this.errorMessage;
   }
 
   public onPinedCbChanged(value:boolean){
     this.pined = value;
   }
 
+
+
   createItem(ListId : ListID, label: string, description : string, date : string, checkbox : boolean) {
-    console.log(checkbox);
-    const itemEdit: dataForItem = {
-      description: description,
-      date : date,
-      pined : checkbox,
-    };
-    const id = this.todoListService.SERVER_CREATE_ITEM(ListId, label, false, itemEdit);
+    console.log(label);
+    if(label === ""){
+      this.errorMessage = "le nom d'une tâche est obligatoire";
+    }
+    else {
+      console.log(label);
+      const itemEdit: dataForItem = {
+        description: description,
+        date: date,
+        pined: checkbox,
+      };
+
+      const id = this.todoListService.SERVER_CREATE_ITEM(ListId, label, false, itemEdit);
+      this.dialogRef.close();
+
+    }
+
+
   }
 
 
